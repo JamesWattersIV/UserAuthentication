52770f4320d4eda0a5a64f5fda435c496207c2a9
JamesWattersIV <53174355+JamesWattersIV@users.noreply.github.com>
2020-01-14 20:19:41 +0200

    Fix error message when page loaded

--- public/js/home.js
+++ public/js/home.js
@@ -10,11 +10,16 @@ function DisplaySignup(){
 
 //JavaScript For Age Slider
 
-let slider = document.getElementById("rangeAge");
+function ageChanged(val){
+    let output = document.getElementById("AgeInput");
+    output.innerHTML = val; // Display the default slider value
+}
+
+/*let slider = document.getElementById("rangeAge");
 let output = document.getElementById("AgeInput");
 output.innerHTML = slider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
     output.innerHTML = this.value;
-}
+}*/
